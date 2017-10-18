31d73b7f04c816e73e6fca7fdadf08a3480f59cd
acoustically <acoustically@kookmin.ac.kr>
2017-10-18 22:28:54 +0900

    add Switch class add button turn on and turn off method

--- main.py
+++ main.py
@@ -4,68 +4,98 @@ import button1_thread
 import button2_thread
 import battery_check_thread
 import wifi_check_thread
-import server_connection_thread
+from server_connection_thread import ServerConnectionThread
 import speach_recognition_thread
 
-message_queue = queue.Queue()
+class Switch:
+    """
+    message
 
-while(1):
-'''
-message
-
-BUTTON_1_TURN_ON
-BUTTON_1_TURN_OFF
-BUTTON_2_TURN_ON 
-BUTTON_2_TURN_OFF
-BATTERY_LOW
-BATTERY_HIGH
-WIFI_LOW
-WIFI_HIGH
-''' 
-    if(!message.empty):
-        msg = message_queue.get()
-        if(msg == message.BUTTON_1_TURN_ON):
-            turn_on_button_1()
-        
-        elif(msg == messge.BUTTON_1_TURN_OFF):
-            turn_off_button_1()
-
-        elif(msg == message.BUTTON_2_TURN_ON):
-            turn_on_button_1()
-
-        elif(msg == message.BUTTON_2_TURN_OFF):
-            turn_off_button_1()
-
-        elif(msg == message.BATTERY_LOW):    
-            turn_down_battery_led()
-
-        elif(msg == message.BATTERY_HIGH):
-            turn_up_battery_led()
-
-        elif(msg == message.WIFI_LOW):    
-            turn_down_wifi_led()
-        
-        elif(msg == message.WIFI_HIGH):    
-            turn_up_wifi_led()
-
-        else:
-
-def turn_on_button_1:
-
-def turn_off_button_1:
-
-def turn_on_button_2:
-
-def turn_off_button_2:
-
-def turn_down_battery_led:
-
-def turn_up_battery_led:
-
-def turn_down_wifi_led:
-
-def turn_up_wifi_led:
+    BUTTON_1_TURN_ON
+    BUTTON_1_TURN_OFF
+    BUTTON_2_TURN_ON 
+    BUTTON_2_TURN_OFF
+    BATTERY_LOW
+    BATTERY_HIGH
+    WIFI_LOW
+    WIFI_HIGH
+    """
+    def __init__(self):
+        self.button1_power = -1
+        self.button2_power = -1
   
+    def start(self):
+        message_queue = queue.Queue()
+        server_connection_thread = ServerConnectionThread(message_queue)
+        server_connection_thread.start()
+        self.process_message(message_queue)
+
+    def process_message(self, message_queue):
+        while 1:
+            if not message_queue.empty():
+                msg = message_queue.get()
+                if msg == message.BUTTON_1_TURN_ON:
+                    self.turn_on_button_1()
+                  
+                elif msg == message.BUTTON_1_TURN_OFF:
+                    self.turn_off_button_1()
+
+                elif msg == message.BUTTON_2_TURN_ON:
+                    self.turn_on_button_2()
+
+                elif msg == message.BUTTON_2_TURN_OFF:
+                    self.turn_off_button_2()
+
+                elif msg == message.BATTERY_LOW:    
+                    self.turn_down_battery_led()
+
+                elif msg == message.BATTERY_HIGH:
+                    self.turn_up_battery_led()
+
+                elif msg == message.WIFI_LOW:    
+                    self.turn_down_wifi_led()
+                
+                elif msg == message.WIFI_HIGH:    
+                    self.turn_up_wifi_led()
+
+    def turn_on_button_1(self):
+        if self.button1_power != 1:
+            self.button1_power = 1
+            print("turn_on_button_1")
+       
+    def turn_off_button_1(self):
+        if self.button1_power != 0:
+            self.button1_power = 0
+            print("turn_off_button_1")
+
+    def turn_on_button_2(self):
+        if self.button2_power != 1:
+            self.button2_power = 1
+            print("turn_on_button_2")
+
+    def turn_off_button_2(self):
+        if self.button2_power != 0:
+            self.button2_power = 0
+            print("turn_off_button_2")
+
+    def turn_down_battery_led():
+        #TODO
+        print("turn_down_battery_led")
+
+    def turn_up_battery_led():
+        #TODO
+        print("turn_up_battery_led")
+
+    def turn_down_wifi_led():
+        #TODO
+        print("turn_down_wifi_led")
+
+    def turn_up_wifi_led():
+        #TODO
+        print("turn_up_wifi_led")
+
+switch = Switch()
+switch.start()
 
 
 
@@ -74,6 +104,4 @@ def turn_up_wifi_led:
 
 
 
-
-
-    
+  
---
+++ server_connection_thread.py
@@ -0,0 +1,38 @@
+from threading import Thread
+import time
+import urllib3
+import message
+import json
+
+class ServerConnectionThread(Thread):
+    def __init__(self, message_queue):
+        Thread.__init__(self)
+        self.message_queue = message_queue
+        self.http = urllib3.PoolManager()
+    
+    def run(self):
+        while 1:
+            json_data = json.dumps({"serial":"0000000001", "token":"acoustically"})
+            res = self.http.request("POST", "http://13.124.7.228:3000/switchs/buttons.json", body=json_data, headers={"Content-Type":"application/json"})
+            res = json.loads(res.data.decode("utf-8"));
+            if res[0]["position"] == 1:
+                if res[0]["power"] == 1:
+                    self.message_queue.put(message.BUTTON_1_TURN_ON)
+                elif res[0]["power"] == 0:
+                    self.message_queue.put(message.BUTTON_1_TURN_OFF)
+                if res[1]["power"] == 1:
+                    self.message_queue.put(message.BUTTON_2_TURN_ON)
+                elif res[1]["power"] == 0:
+                    self.message_queue.put(message.BUTTON_2_TURN_OFF)
+            elif res[0]["position"] == 2:
+                if res[0]["power"] == 1:
+                    self.message_queue.put(message.BUTTON_2_TURN_ON)
+                elif res[0]["power"] == 0:
+                    self.message_queue.put(message.BUTTON_2_TURN_OFF)
+                if res[1]["power"] == 1:
+                    self.message_queue.put(message.BUTTON_1_TURN_ON)
+                elif res[1]["power"] == 0:
+                    self.message_queue.put(message.BUTTON_1_TURN_OFF)
+                
+            time.sleep(0.5)
+            
